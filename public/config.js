447e89dc61abe406e34615f71b85cdf42003e7b1
KiyotoHirai-hub <kiyoto.kiyokyt@gmail.com>
2026-04-29 09:53:24 +0900

    supabase url

--- public/config.js
+++ public/config.js
@@ -7,6 +7,6 @@
  * 3. 下の SUPABASE_URL と SUPABASE_ANON_KEY に貼り付ける
  */
 
-const SUPABASE_URL      = 'https://xxxxxxxxxxx.supabase.co'; // ← 自分のURLに変更
-const SUPABASE_ANON_KEY = 'eyJxxxxxxxxxx';                   // ← 自分のキーに変更
+const SUPABASE_URL      = 'https://twkutqecieqmdhoqnjgt.supabase.co'; // ← 自分のURLに変更
+const SUPABASE_ANON_KEY = 'sb_publishable_FI0pNUiJx07KFsCG5Qr8BA_CWgS8fG-';                   // ← 自分のキーに変更
 const STORAGE_BUCKET    = 'item-photos';
